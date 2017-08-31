7f2d58d1a427d54519b583a77d2fe4c4a38344a1
wordsforthewise <wordsforthewise@gmail.com>
2017-08-31 00:36:35 -0600

    FEAT: add get_home_dir()

--- code/utils.py
+++ code/utils.py
@@ -1,3 +1,17 @@
+import os
+
+def get_home_dir(repo='crypto_predict'):
+    cwd = os.getcwd()
+    cwd_list = cwd.split('/')
+    repo_position = [i for i, s in enumerate(cwd_list) if s == repo]
+    if len(repo_position) > 1:
+        print("error!  more than one intance of repo name in path")
+        return None
+
+    home_dir = '/'.join(cwd_list[:repo_position[0] + 1]) + '/'
+    return home_dir
+
+
 def pull_s3():
     """
     Syncs all data from amazon S3.
