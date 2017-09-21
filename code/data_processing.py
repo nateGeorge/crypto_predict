55514c22f01ccd8df4badefbb8d8d19f4c332d66
nateGeorge <wordsforthewise@gmail.com>
2017-09-20 21:51:31 -0600

    FEAT: fix up rescale and prototype future predictions of arbitrary num of preds

--- code/data_processing.py
+++ code/data_processing.py
@@ -42,6 +42,7 @@ def transform_data(df, mva=30):
         new_df_dict[c] = scaled[:, 0]
 
     new_df = pd.DataFrame(new_df_dict)
+    new_df.set_index(df.index, inplace=True)
 
     return new_df, scalers
 
@@ -54,7 +55,12 @@ def reform_data(df, scaled_df, scalers, mva=30):
     unsc_dict = {}
     for c in scaled_df.columns:
         unscaled = scalers[c].inverse_transform(scaled_df[c])
-        rolling_mean = df[c].rolling(window=mva).mean().bfill()
+        if scaled_df.shape[0] <= mva:
+            rolling_mean = df.iloc[-(scaled_df.shape[0] + mva):][c].rolling(window=mva).mean().bfill()
+            rolling_mean = rolling_mean[-scaled_df.shape[0]:]
+        else:
+            rolling_mean = df[c].rolling(window=mva).mean().bfill()
+
         unsc = unscaled * rolling_mean
         unsc_dict[c] = unsc
 
@@ -88,19 +94,25 @@ def reform_prediction(df, prediction, mva=30):
                 not scaled or normalized
     :param prediction: rescaled
     """
-    return df.iloc[-mva + 1:].close.sum() / (mva - prediction)
+    return df.iloc[-mva + 1:].close.sum() / (mva / prediction - 1)
 
 
 def reform_future_predictions(df, preds, mva=30):
     """
     rescales future predictions
     :param df: pandas dataframe of current data, up to most recent timestep before future predictions
-    :param preds: numpy array of predictions, with shape (x,)
+    :param preds: numpy array of predictions, with shape (x,), should be rescaled (i.e. all positive)
     :param mva: integeter specifying moving average period (same as used for scaling the data)
     """
     resc_preds = [reform_prediction(df, preds[0], mva=mva)]
-    for i in range(1, mva):
-        cur = (df.iloc[-mva + 1 + i:].close.sum() + sum(resc_preds[:i])) / (mva - preds[i])
+    # optional: set the first point to the last known point
+    # resc_preds = [df.close[-mva]]
+    for i in range(1, mva - 1):
+        cur = (df.iloc[-mva + i + 1:].close.sum() + sum(resc_preds)) / (mva / preds[i] - 1)
+        resc_preds.append(cur)
+
+    for i in range(mva - 1, preds.shape[0]):
+        cur = sum(resc_preds[-mva + i + 1:i]) / (mva / preds[i] - 1)
         resc_preds.append(cur)
 
     return pd.DataFrame({'close': resc_preds})
